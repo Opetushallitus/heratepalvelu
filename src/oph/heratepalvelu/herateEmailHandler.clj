c831ff15ff96973311df775e5145bb231994955c
Joona Haavisto <joona.haavisto@gofore.com>
2019-07-31 13:43:35 +0300

    Ei lähetetä vanhetuneita kyselylinkkejä

--- src/oph/heratepalvelu/herateEmailHandler.clj
+++ src/oph/heratepalvelu/herateEmailHandler.clj
@@ -2,8 +2,9 @@
   (:require [oph.heratepalvelu.db.dynamodb :as ddb]
             [oph.heratepalvelu.external.viestintapalvelu :refer [send-email]]
             [clojure.tools.logging :as log]
-            [clj-time.core :refer [today now]]
-            [cheshire.core :refer [parse-string]])
+            [clj-time.core :as t]
+            [cheshire.core :refer [parse-string]]
+            [clojure.string :as str])
   (:import (software.amazon.awssdk.awscore.exception AwsServiceException)))
 
 (gen-class
@@ -12,9 +13,16 @@
              [com.amazonaws.services.lambda.runtime.events.ScheduledEvent
               com.amazonaws.services.lambda.runtime.Context] void]])
 
+(defn has-time-to-answer? [alkupvm]
+  (let [[years months days] (map #(Integer. %)
+                                 (str/split alkupvm #"-"))]
+    (t/before? (t/today)
+               (t/plus (t/local-date years months days)
+                       (t/days 30)))))
+
 (defn -handleSendEmails [this event context]
   (loop [lahetettavat (ddb/query-items {:lahetystila [:eq [:s "ei_lahetetty"]]
-                                        :alkupvm     [:le [:s (.toString (today))]]}
+                                        :alkupvm     [:le [:s (.toString (t/today))]]}
                                        {:index "lahetysIndex"
                                         :limit 100})]
     (log/info "Käsitellään " (count lahetettavat) " lähetettävää viestiä.")
@@ -24,15 +32,24 @@
           (let [time (System/currentTimeMillis)
                 id (:id (send-email email))]
             (try
-              (ddb/update-item
-                {:toimija_oppija [:s (:toimija_oppija email)]
-                 :tyyppi_kausi   [:s (:tyyppi_kausi email)]}
-                {:update-expr     (str "SET #lahetystila = :lahetystila, "
-                                       "SET #vp-id = :vp-id")
-                 :expr-attr-names {"#lahetystila" "lahetystila"
-                                   "#vp-id" "viestintapalvelu-id"}
-                 :expr-attr-vals  {":lahetystila" [:s (str "viestintapalvelussa")]
-                                   ":vp-id" [:n id]}})
+              (if (has-time-to-answer? (:alkupvm email))
+                (ddb/update-item
+                  {:toimija_oppija [:s (:toimija_oppija email)]
+                   :tyyppi_kausi   [:s (:tyyppi_kausi email)]}
+                  {:update-expr     (str "SET #lahetystila = :lahetystila, "
+                                         "SET #vp-id = :vp-id")
+                   :expr-attr-names {"#lahetystila" "lahetystila"
+                                     "#vp-id" "viestintapalvelu-id"}
+                   :expr-attr-vals  {":lahetystila"
+                                     [:s "viestintapalvelussa"]
+                                     ":vp-id" [:n id]}})
+                (ddb/update-item
+                  {:toimija_oppija [:s (:toimija_oppija email)]
+                   :tyyppi_kausi   [:s (:tyyppi_kausi email)]}
+                  {:update-expr     "SET #lahetystila = :lahetystila"
+                   :expr-attr-names {"#lahetystila" "lahetystila"}
+                   :expr-attr-vals {":lahetystila"
+                                    [:s "vastausaika_loppunut_ennen_lahetysta"]}}))
               (catch AwsServiceException e
                 (log/error "Viesti " id " lähetty viestintäpalveluun, muttei päivitetty kantaan!")
                 (log/error e)))
@@ -43,6 +60,6 @@
             (log/error e))))
       (when (> 30000 (.getRemainingTimeInMillis context))
         (recur (ddb/query-items {:lahetystila [:eq [:s "ei_lahetetty"]]
-                                 :alkupvm     [:le [:s (.toString (today))]]}
+                                 :alkupvm     [:le [:s (.toString (t/today))]]}
                                 {:index "lahetysIndex"
                                  :limit 100}))))))
---
+++ test/oph/heratepalvelu/herateEmailHandler_test.clj
@@ -0,0 +1,19 @@
+(ns oph.heratepalvelu.herateEmailHandler-test
+  (:require [clojure.test :refer :all]
+            [oph.heratepalvelu.herateEmailHandler :as h]
+            [clj-time.core :as t]
+            [clj-time.format :as f]))
+
+(deftest test-has-time-to-answer
+  (let [date1 (t/plus  (t/today) (t/days 10))
+        date2 (t/minus (t/today) (t/days 40))
+        date3 (t/plus  (t/today) (t/days 29))
+        date4 (t/minus (t/today) (t/days 30))]
+    (is (= true (h/has-time-to-answer?
+                  (f/unparse-local-date (:date f/formatters) date1))))
+    (is (= false (h/has-time-to-answer?
+                  (f/unparse-local-date (:date f/formatters) date2))))
+    (is (= true (h/has-time-to-answer?
+                  (f/unparse-local-date (:date f/formatters) date3))))
+    (is (= false (h/has-time-to-answer?
+                  (f/unparse-local-date (:date f/formatters) date4))))))
