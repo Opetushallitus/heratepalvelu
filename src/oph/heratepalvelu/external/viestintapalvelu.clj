453d793b672840b7a316173d31be22829015f477
Joona Haavisto <joona.haavisto@gofore.com>
2019-09-12 15:38:36 +0300

    Lisätty paragraph linkki-elementtien ympärille

--- src/oph/heratepalvelu/external/viestintapalvelu.clj
+++ src/oph/heratepalvelu/external/viestintapalvelu.clj
@@ -15,21 +15,21 @@
    [:p "Henkilökohtainen osaamisen kehittämissuunnitelmasi (HOKS) on nyt hyväksytty ja opinnot alkamassa. Onnistunut aloitus ja suunnitelma luovat hyvän pohjan tavoitteiden saavuttamiselle. Siksi kokemuksesi ovat tärkeitä. Kerro meille, missä olemme onnistuneet ja mitä voisimme tehdä vielä paremmin."]
    [:p "Toivomme, että käyttäisit noin 10-15 minuuttia aikaa tähän kyselyyn vastaamiseen. Kysymykset koskevat ammatillisen koulutuksen aloitusvaihetta, HOKSin laadintaa ja opiskeluilmapiiriä."]
    [:p "Palaute annetaan nimettömänä. Vastauksiasi käytetään ammatillisen koulutuksen kehittämiseen."]
-   [:a {:href link} link]
+   [:p [:a {:href link} link]]
    [:p "Terveisin oppilaitoksesi"]
    horizontal-line
    [:p "Bästa studerande!"]
    [:p "Din personliga utvecklingsplan för kunnandet (PUK) har nu godkänts och studierna ska snart börja. En lyckad studiestart och god planering skapar en bra grund för att du ska kunna nå dina mål. Därför är det viktigt att vi får höra dina erfarenheter och kommentarer. Vi vill gärna höra dina åsikter om hur vi har lyckats och vad vi kunde göra bättre i fortsättningen."]
    [:p "Vi hoppas att du tar dig tid att svara på den här enkäten. Det tar ungefär 10–15 minuter att svara. Frågorna berör de inledande studierna, uppgörandet av PUK och studieklimatet."]
    [:p "Responsen ges anonymt. Dina svar används för att utveckla yrkesutbildningen."]
-   [:a {:href link} link]
+   [:p [:a {:href link} link]]
    [:p "Hälsningar, din läroanstalt"]
    horizontal-line
    [:p "Dear student!"]
    [:p "Your personal competence development plan has now been approved and you are about to begin your studies. A successful beginning and a sound plan lay a good foundation for achieving your goals. This is why your experiences matter. Tell us what we did well and what we could do even better."]
    [:p "We would like you to spend some 10 to 15 minutes on responding to this survey. The questions concern the starting phase of vocational education and training, the preparation of your personal competence plan, and the study atmosphere."]
    [:p "You can give your feedback anonymously. Your responses will be used to develop vocational education and training."]
-   [:a {:href link} link]
+   [:p [:a {:href link} link]]
    [:p "With best regards, your educational institution"]])
 
 (defn- amispalaute-body-loppukysely [link]
@@ -40,7 +40,7 @@
    [:p "Toivomme, että käyttäisit noin 10-15 minuuttia aikaa tähän kyselyyn vastaamiseen."]
    [:p "Palaute annetaan nimettömänä. Vastauksiasi käytetään ammatillisen koulutuksen kehittämiseen."]
    [:p "Kiitos, että vastaat!"]
-   [:a {:href link} link]
+   [:p [:a {:href link} link]]
    [:p "Terveisin oppilaitoksesi"]
    horizontal-line
    [:p "Hej!"]
@@ -49,7 +49,7 @@
    [:p "Vi hoppas att du tar dig tid att svara på den här enkäten. Det tar ungefär 10–15 minuter att svara."]
    [:p "Responsen ges anonymt. Dina svar används för att utveckla yrkesutbildningen."]
    [:p "Tack för att du svarar!"]
-   [:a {:href link} link]
+   [:p [:a {:href link} link]]
    [:p "Hälsningar, din läroanstalt"]
    horizontal-line
    [:p "Dear graduate!"]
@@ -58,7 +58,7 @@
    [:p "We would like you to spend some 10 to 15 minutes on responding to this survey."]
    [:p "You can give your feedback anonymously. Your responses will be used to develop vocational education and training."]
    [:p "Thank you for responding!"]
-   [:a {:href link} link]
+   [:p [:a {:href link} link]]
    [:p "With best regards, your educational institution"]])
 
 (defn- amispalaute-html [data]
