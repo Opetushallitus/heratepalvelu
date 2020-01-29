ff01d306e0f3f073dc67081bddb79343adc1eea6
Joona Haavisto <joona.haavisto@gofore.com>
2020-01-29 12:47:10 +0200

    Lisätty deploy script testi & build automaatioksi

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+env=$1
+qa="pallero"
+prod="sade"
+
+echo "$env"
+echo "$qa"
+
+if lein test ; then
+  lein uberjar
+  cd cdk || exit $?
+  if [ "$env" == "$prod" ]; then
+    aws-vault exec oph-prod -- cdk deploy sade-services-heratepalvelu
+  elif [ "$env" == "$qa" ]; then
+    aws-vault exec oph-dev -- cdk deploy pallero-services-heratepalvelu
+  else
+    aws-vault exec oph-dev -- cdk deploy sieni-services-heratepalvelu
+  fi
+fi
