c58cc00cf291f2c8bdc66c6d3ec733fb6f5a82ea
Joona Haavisto <joona.haavisto@gofore.com>
2020-02-21 15:23:04 +0200

    Poistettu duplikaatti rivejä

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -20,22 +20,19 @@ const version = repo.getReferenceTarget(repo.getHead());
 
 const app = new cdk.App();
 
-if (canDeploy) {
-  new HeratepalveluAMISStack(app, "sieni-services-heratepalvelu", "sieni", version);
-  new HeratepalveluAMISStack(app, "pallero-services-heratepalvelu", "pallero", version);
+new HeratepalveluAMISStack(app, "sieni-services-heratepalvelu", "sieni", version);
+new HeratepalveluTEPStack(app, "sieni-services-heratepalvelu-tep", "sieni", version);
 
-  new HeratepalveluTEPStack(app, "sieni-services-heratepalvelu-tep", "sieni", version);
+if (canDeploy) {
+  new HeratepalveluAMISStack(app, "pallero-services-heratepalvelu", "pallero", version);
   new HeratepalveluTEPStack(app, "pallero-services-heratepalvelu-tep", "pallero", version);
 
   if (upstreamBranch === "refs/remotes/origin/master") {
-    new HeratepalveluAMISStack(app, "sade-services-heratepalvelu", "", version);
+    new HeratepalveluAMISStack(app, "sade-services-heratepalvelu", "sade", version);
     new HeratepalveluTEPStack(app, "sade-services-heratepalvelu-tep", "sade", version);
   }
 } else {
   console.log("Uncommited changes or local is ahead/behind of remote:\n");
   console.log(status);
   console.log(aheadBehindCount);
-
-  new HeratepalveluAMISStack(app, "sieni-services-heratepalvelu", 'sieni', version);
-  new HeratepalveluTEPStack(app, "sieni-services-heratepalvelu-tep", "sieni", version);
 }
