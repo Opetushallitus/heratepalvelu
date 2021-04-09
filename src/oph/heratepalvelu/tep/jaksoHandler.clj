666af3fa767335169b8efce7e210df1b7300ef6d
Joona Haavisto <joona.haavisto@gofore.com>
2021-04-10 02:34:41 +0300

    Testi logitus

--- src/oph/heratepalvelu/tep/jaksoHandler.clj
+++ src/oph/heratepalvelu/tep/jaksoHandler.clj
@@ -98,6 +98,7 @@
                        :tallennuspvm         [:s (str (t/today))]
                        :tutkinnonosa-tyyppi  [:s (:tyyppi herate)]
                        :tutkinnonosa-id      [:n (:tutkinnonosa-id herate)]}]
+          (log/info arvo-resp)
           (try
             (ddb/put-item
               (cond-> db-data
