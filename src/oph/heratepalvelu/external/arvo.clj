a963fe38f810f7a52f8b0c240f859c82c4c27adf
Joona Haavisto <joona.haavisto@gofore.com>
2021-04-23 17:58:57 +0300

    testi logitus

--- src/oph/heratepalvelu/external/arvo.clj
+++ src/oph/heratepalvelu/external/arvo.clj
@@ -193,6 +193,7 @@
                   :body         (generate-string data)
                   :basic-auth   [(:arvo-user env) @pwd]
                   :as           :json})]
+      (log/info resp)
       (:body resp))
     (catch ExceptionInfo e
       (log/error e)
