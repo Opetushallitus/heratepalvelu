ffd5e69ffeae657007ec4694c125e4bf89bcf530
Joona Haavisto <joona.haavisto@gofore.com>
2021-07-15 17:34:22 +0300

    Korjattu ehtolausekkeiden järjestys

--- src/oph/heratepalvelu/tep/emailHandler.clj
+++ src/oph/heratepalvelu/tep/emailHandler.clj
@@ -42,12 +42,14 @@
           nil))))
 
 (defn- sms-lahetysosoite [email jaksot]
-  (let [phone (:ohjaaja_puhelinnumero (reduce #(if (and (some? (:ohjaaja_puhelinnumero %1))
-                                                         (= (:ohjaaja_puhelinnumero %1)
-                                                            (:ohjaaja_puhelinnumero %2)))
-                                                  %1
-                                                  (reduced nil))
-                                               jaksot))]
+  (let [phone (:ohjaaja_puhelinnumero (reduce #(if (some? (:ohjaaja_puhelinnumero %1))
+                                                 (if (some? (:ohjaaja_puhelinnumero %2))
+                                                   (if (= (:ohjaaja_puhelinnumero %1) (:ohjaaja_puhelinnumero %2))
+                                                     %1
+                                                     (reduced nil))
+                                                   %1)
+                                                 %2)
+                                              jaksot))]
     (if (some? phone)
       phone
       (do (log/warn "Ei yksiselitteistä ohjaajan puhelinnumeroa "
@@ -57,9 +59,9 @@
           (ddb/update-item
             {:ohjaaja_ytunnus_kj_tutkinto [:s (:ohjaaja_ytunnus_kj_tutkinto email)]
              :niputuspvm                  [:s (:niputuspvm email)]}
-            {:update-expr     (str "SET #sms-kasittelytila = :sms-kasittelytila")
-             :expr-attr-names {"#sms-kasittelytila" "sms_kasittelytila"}
-             :expr-attr-vals {":sms-kasittelytila" [:s (:phone-mismatch c/kasittelytilat)]}}
+            {:update-expr     (str "SET #sms_kasittelytila = :sms_kasittelytila")
+             :expr-attr-names {"#sms_kasittelytila" "sms_kasittelytila"}
+             :expr-attr-vals {":sms_kasittelytila" [:s (:phone-mismatch c/kasittelytilat)]}}
             (:nippu-table env))
           nil))))
 
@@ -85,75 +87,76 @@
               puhelinnumero (sms-lahetysosoite email jaksot)
               sms-kasittelytila (:sms_kasittelytila email)]
           (if (c/has-time-to-answer? (:voimassaloppupvm email))
-            (when (some? osoite)
-              (try
-                (let [id (:id (vp/send-email {:subject "Työpaikkaohjaajakysely - Enkät till arbetsplatshandledaren - Survey to workplace instructors"
-                                              :body (vp/tyopaikkaohjaaja-html
-                                                      email
-                                                      oppilaitokset)
-                                              :address osoite
-                                              :sender "OPH – UBS"}))
-                      lahetyspvm (str (t/today))]
+            (do
+              (when (some? osoite)
+                (try
+                  (let [id (:id (vp/send-email {:subject "Työpaikkaohjaajakysely - Enkät till arbetsplatshandledaren - Survey to workplace instructors"
+                                                :body (vp/tyopaikkaohjaaja-html
+                                                        email
+                                                        oppilaitokset)
+                                                :address osoite
+                                                :sender "OPH – UBS"}))
+                        lahetyspvm (str (t/today))]
+                    (ddb/update-item
+                      {:ohjaaja_ytunnus_kj_tutkinto [:s (:ohjaaja_ytunnus_kj_tutkinto email)]
+                       :niputuspvm                  [:s (:niputuspvm email)]}
+                      {:update-expr     (str "SET #kasittelytila = :kasittelytila, "
+                                             "#vpid = :vpid, "
+                                             "#lahetyspvm = :lahetyspvm, "
+                                             "#muistutukset = :muistutukset, "
+                                             "#lahetysosoite = :lahetysosoite")
+                       :expr-attr-names {"#kasittelytila" "kasittelytila"
+                                         "#vpid" "viestintapalvelu-id"
+                                         "#lahetyspvm" "lahetyspvm"
+                                         "#muistutukset" "muistutukset"
+                                         "#lahetysosoite" "lahetysosoite"}
+                       :expr-attr-vals  {":kasittelytila" [:s (:viestintapalvelussa c/kasittelytilat)]
+                                         ":vpid" [:n id]
+                                         ":lahetyspvm" [:s lahetyspvm]
+                                         ":muistutukset" [:n 0]
+                                         ":lahetysosoite" [:s osoite]}}
+                      (:nippu-table env)))
+                  (catch AwsServiceException e
+                    (log/error "Viesti " email " lähetty viestintäpalveluun, muttei päivitetty kantaan!")
+                    (log/error e))
+                  (catch Exception e
+                    (log/error "Virhe viestin lähetyksessä!" email)
+                    (log/error e))))
+              (when (and (some? puhelinnumero)
+                         (or (nil? sms-kasittelytila)
+                             (= sms-kasittelytila (:ei-lahetetty c/kasittelytilat))))
+                (try
+                  (sqs/send-tep-sms-sqs-message (sqs/build-sms-sqs-message
+                                                  oppilaitokset
+                                                  puhelinnumero
+                                                  (:ohjaaja_ytunnus_kj_tutkinto email)
+                                                  (:niputuspvm email)))
                   (ddb/update-item
                     {:ohjaaja_ytunnus_kj_tutkinto [:s (:ohjaaja_ytunnus_kj_tutkinto email)]
                      :niputuspvm                  [:s (:niputuspvm email)]}
-                    {:update-expr     (str "SET #kasittelytila = :kasittelytila, "
-                                           "#vpid = :vpid, "
-                                           "#lahetyspvm = :lahetyspvm, "
-                                           "#muistutukset = :muistutukset, "
-                                           "#lahetysosoite = :lahetysosoite")
-                     :expr-attr-names {"#kasittelytila" "kasittelytila"
-                                       "#vpid" "viestintapalvelu-id"
-                                       "#lahetyspvm" "lahetyspvm"
-                                       "#muistutukset" "muistutukset"
-                                       "#lahetysosoite" "lahetysosoite"}
-                     :expr-attr-vals  {":kasittelytila" [:s (:viestintapalvelussa c/kasittelytilat)]
-                                       ":vpid" [:n id]
-                                       ":lahetyspvm" [:s lahetyspvm]
-                                       ":muistutukset" [:n 0]
-                                       ":lahetysosoite" [:s osoite]}}
-                    (:nippu-table env)))
-                (catch AwsServiceException e
-                  (log/error "Viesti " email " lähetty viestintäpalveluun, muttei päivitetty kantaan!")
-                  (log/error e))
-                (catch Exception e
-                  (log/error "Virhe viestin lähetyksessä!" email)
-                  (log/error e)))
-              (try
-                (ddb/update-item
-                  {:ohjaaja_ytunnus_kj_tutkinto [:s (:ohjaaja_ytunnus_kj_tutkinto email)]
-                   :niputuspvm                  [:s (:niputuspvm email)]}
-                  {:update-expr     (str "SET #kasittelytila = :kasittelytila, "
-                                         "#lahetyspvm = :lahetyspvm")
-                   :expr-attr-names {"#kasittelytila" "kasittelytila"
-                                     "#lahetyspvm" "lahetyspvm"}
-                   :expr-attr-vals {":kasittelytila" [:s (:vastausaika-loppunut c/kasittelytilat)]
-                                    ":lahetyspvm" [:s (str (t/today))]}}
-                  (:nippu-table env))
-                (catch Exception e
-                  (log/error "Virhe lähetystilan päivityksessä nipulle, jonka vastausaika umpeutunut" email)
-                  (log/error e))))
-            (when (and (some? puhelinnumero)
-                       (or (nil? sms-kasittelytila)
-                           (= sms-kasittelytila (:ei-lahetetty c/kasittelytilat))))
-              (try
-                (sqs/send-tep-sms-sqs-message (sqs/build-sms-sqs-message
-                                                oppilaitokset
-                                                puhelinnumero
-                                                (:ohjaaja_ytunnus_kj_tutkinto email)
-                                                (:niputuspvm email)))
-                (ddb/update-item
-                  {:ohjaaja_ytunnus_kj_tutkinto [:s (:ohjaaja_ytunnus_kj_tutkinto email)]
-                   :niputuspvm                  [:s (:niputuspvm email)]}
-                  {:update-expr     (str "SET #sms-kasittelytila = :sms-kasittelytila")
-                   :expr-attr-names {"#sms-kasittelytila" "sms_kasittelytila"}
-                   :expr-attr-vals {":sms-kasittelytila" [:s (:queued c/kasittelytilat)]}}
-                  (:nippu-table env))
-              (catch AwsServiceException e
-                (log/error (str "SMS-viestin tilan päivityksessä tapahtunut virhe! Numero: " puhelinnumero))
-                (log/error e))
+                    {:update-expr     (str "SET #sms_kasittelytila = :sms_kasittelytila")
+                     :expr-attr-names {"#sms_kasittelytila" "sms_kasittelytila"}
+                     :expr-attr-vals {":sms_kasittelytila" [:s (:queued c/kasittelytilat)]}}
+                    (:nippu-table env))
+                  (catch AwsServiceException e
+                    (log/error (str "SMS-viestin tilan päivityksessä tapahtunut virhe! Numero: " puhelinnumero))
+                    (log/error e))
+                  (catch Exception e
+                    (log/error e)))))
+            (try
+              (ddb/update-item
+                {:ohjaaja_ytunnus_kj_tutkinto [:s (:ohjaaja_ytunnus_kj_tutkinto email)]
+                 :niputuspvm                  [:s (:niputuspvm email)]}
+                {:update-expr     (str "SET #kasittelytila = :kasittelytila, "
+                                       "#lahetyspvm = :lahetyspvm")
+                 :expr-attr-names {"#kasittelytila" "kasittelytila"
+                                   "#lahetyspvm" "lahetyspvm"}
+                 :expr-attr-vals {":kasittelytila" [:s (:vastausaika-loppunut c/kasittelytilat)]
+                                  ":lahetyspvm" [:s (str (t/today))]}}
+                (:nippu-table env))
               (catch Exception e
-                (log/error e))))))))
+                (log/error "Virhe lähetystilan päivityksessä nipulle, jonka vastausaika umpeutunut" email)
+                (log/error e)))))))
       (when (< 60000 (.getRemainingTimeInMillis context))
         (recur (ddb/query-items {:kasittelytila [:eq [:s (:ei-lahetetty c/kasittelytilat)]]
                                  :niputuspvm    [:le [:s (str (t/today))]]}
