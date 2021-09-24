dcc56da63b6e15d79781e0df595ab3b6f92d9f9b
Kelvin Jackson <kelvin.jackson@gofore.com>
2021-09-24 16:33:41 +0300

    Enemmän toiminnallisuutta lisätty; valmis ehkä 27.9

--- src/oph/heratepalvelu/tpk/niputusHandler.clj
+++ src/oph/heratepalvelu/tpk/niputusHandler.clj
@@ -5,8 +5,10 @@
             [oph.heratepalvelu.db.dynamodb :as ddb]
             [oph.heratepalvelu.external.arvo :as arvo]
             [oph.heratepalvelu.log.caller-log :refer :all])
-  (:import (com.amazonaws.services.lambda.runtime Context)
+  (:import (clojure.lang ExceptionInfo)
+           (com.amazonaws.services.lambda.runtime Context)
            (com.amazonaws.services.lambda.runtime.events ScheduledEvent)
+           (java.time LocalDate)
            (software.amazon.awssdk.awscore.exception AwsServiceException)))
 
 (gen-class :name "oph.heratepalvelu.tpk.tpkNiputusHandler"
@@ -16,12 +18,16 @@
     ;; TODO palauttaa true, jos jaksossa on tarvittavia tietoja
   )
 
-(defn create-nippu-id [jakso]
+(defn get-kausi [jakso]
   (let [loppupvm (c/to-date (:jakso_loppupvm jakso))]
-    (str (c/normalize-string (:tyopaikan_nimi jakso)) "/"
-         (:koulutustoimija jakso) "/"
-         (.getYear loppupvm) ;; kl/sl = kevät/syksy -lukukausi
+    (str (.getYear loppupvm) ;; kl/sl = kevät-/syyslukukausi
          (if (<= (.getMonthValue loppupvm) 6) "kl" "sl"))))
+
+(defn create-nippu-id [jakso]
+  (str (c/normalize-string (:tyopaikan_nimi jakso)) "/"
+       (:tyopaikan_ytunnus jakso) "/"
+       (:koulutustoimija jakso) "/"
+       (get-kausi jakso)))
 
 (defn- get-existing-nippu [jakso]
   (try
@@ -30,21 +36,38 @@
     (catch AwsServiceException e
       nil)))
 
+(defn get-next-vastaamisajan-alkupvm-date [jakso]
+  (let [loppupvm (c/to-date (:jakso_loppupvm jakso))
+        year (.getYear loppupvm)
+        kausi-month (if (<= (.getMonthValue loppupvm) 6) 7 1)
+        kausi-year (if (= kausi-month 1) (+ year 1) year)]
+    (LocalDate/of kausi-year kausi-month 1)))
+
 (defn create-nippu [jakso]
-  ;; TODO
-  )
+  (let [alkupvm (get-next-vastaamisajan-alkupvm-date jakso)
+        loppupvm (.minusDays (.plusMonths alkupvm 2) 1)]
+    {:nippu-id               (create-nippu-id jakso)
+     :koulutustoimija_oid    (:koulutustoimija jakso)
+     :tyonantaja             (:tyopaikan_nimi jakso)
+     :tyopaikka              (:tyopaikan_ytunnus jakso)
+     :vastaamisajan_alkupvm  (str alkupvm)
+     :vastaamisajan_loppupvm (str loppupvm)
+     :kausi                  (get-kausi jakso)}))
+
 
 (defn- save-nippu [nippu]
   (try
-    ;; TODO toimii nyt, kun emme päivitä olemassaolevia nippuja. Myöhemmin tämä
-    ;; täytyy korvata update-item -funktiolla.
     (ddb/put-item nippu {} (:tpk-nippu-table env))
     (catch AwsServiceException e
       (log/error "Virhe DynamoDB tallennuksessa (TPK):" e))))
 
 (defn- get-kyselylinkki [nippu]
-  ;; TODO + error handling
-  )
+  (try
+    (let [body (arvo/create-tpk-kyselylinkki (dissoc nippu :nippu-id))]
+      (when body
+        (:kyselylinkki body))) ;; TODO en ole varma tästä nimestä
+    (catch ExceptionInfo e
+      nil)))
 
 (defn- query-niputtamattomat []
   (ddb/query-items {:tpk-niputuspvm [:null]
@@ -54,7 +77,6 @@
                    (:jaksotunnus-table env)))
 
 ;; TODO suodata pois yrittäjien oppisopimukset
-;; TODO Onko kaikki oppisopimukset yrittäjien? Todennäköisesti ei.
 (defn -handleTpkNiputus [this event context]
   (log-caller-details-scheduled "handleTpkNiputus" event context)
   (loop [niputettavat (query-niputtamattomat)]
@@ -63,11 +85,10 @@
       (doseq [jakso niputettavat]
         (when (check-jakso? jakso)
           (let [existing-nippu (get-existing-nippu jakso)]
-            (if existing-nippu
-              (do) ;; TODO skip for now if nippu already exists
+            (when-not existing-nippu
               (let [nippu (create-nippu jakso)
                     kyselylinkki (get-kyselylinkki nippu)]
-                (when kyselylinkki ;; TODO if save-nippu returns nil, delete kyselylinkki
+                (when kyselylinkki ;; TODO if save-nippu returns nil, delete kyselylinkki (or reverse?)
                   (save-nippu (assoc nippu :kyselylinkki kyselylinkki))))))))
       (when (< 120000 (.getRemainingTimeInMillis context))
         (recur (query-niputtamattomat))))))
