3a2fd1e37b02b21cef05e2d93e133d4ff5629510
Kelvin Jackson <80880604+kechpaja-at-gofore@users.noreply.github.com>
2021-10-14 13:23:19 +0300

    Logitus lisätty EH-1229:n tutkimukseen (#49)

--- src/oph/heratepalvelu/amis/AMISherateHandler.clj
+++ src/oph/heratepalvelu/amis/AMISherateHandler.clj
@@ -34,7 +34,9 @@
           (if (and
                 (:status (ex-data e))
                 (= 404 (:status (ex-data e))))
-            (log/error "Ei opiskeluoikeutta " (:opiskeluoikeus-oid
-                                                (parse-string (.getBody msg) true)))
+            (log/error "404-virhe. Opiskeluoikeus: "
+                       (:opiskeluoikeus-oid (parse-string (.getBody msg) true))
+                       " error: "
+                       e)
             (do (log/error e)
                 (throw e))))))))
--- src/oph/heratepalvelu/external/organisaatio.clj
+++ src/oph/heratepalvelu/external/organisaatio.clj
@@ -1,7 +1,12 @@
 (ns oph.heratepalvelu.external.organisaatio
   (:require [oph.heratepalvelu.external.http-client :as client]
             [environ.core :refer [env]]
-            [clojure.tools.logging :as log]))
+            [clojure.tools.logging :as log])
+  (:import (clojure.lang ExceptionInfo)))
 
 (defn get-organisaatio [oid]
-  (:body (client/get (str (:organisaatio-url env) oid) {:as :json})))
+  (try
+    (:body (client/get (str (:organisaatio-url env) oid) {:as :json}))
+    (catch ExceptionInfo e
+      (do (log/error "Virhe hausta organisaatiopalvelusta:" e)
+          (throw e)))))
