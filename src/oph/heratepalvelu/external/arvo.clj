9a1caed5a43a4a4a28fd60e63a45ab586539bca3
samje <sami.honkanen@cybercom.com>
2021-11-22 14:17:48 +0200

    1.7. alkupvm-rajoitus myos UpdatedOOHandleriin. Kommentoitu tyopaikan normalisointi pois toistaiseksi.

--- src/oph/heratepalvelu/external/arvo.clj
+++ src/oph/heratepalvelu/external/arvo.clj
@@ -144,7 +144,7 @@
   {:koulutustoimija_oid       koulutustoimija
    :tyonantaja                (:tyopaikan-ytunnus herate)
    :tyopaikka                 (:tyopaikan-nimi herate)
-   :tyopaikka_normalisoitu    tyopaikka-normalisoitu
+   ;; :tyopaikka_normalisoitu    tyopaikka-normalisoitu Laitetaan takaisin, kun Arvo on valmis
    :tutkintotunnus            (get-in
                                 suoritus
                                 [:koulutusmoduuli
