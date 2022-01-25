d384298be13bfae452f521a64d23afb4dcbc347b
Kelvin Jackson <kelvin.jackson@gofore.com>
2022-01-25 16:38:23 +0200

    Lisätty yksi testi

--- test/oph/heratepalvelu/external/ehoks_test.clj
+++ test/oph/heratepalvelu/external/ehoks_test.clj
@@ -14,6 +14,17 @@
 
 (deftest test-get-hoks-by-opiskeluoikeus
   (testing "Varmista, että get-hoks-by-opiskeluoikeus toimii oikein"
-    (with-redefs [oph.heratepalvelu.external.cas-client/get-service-ticket
+    (with-redefs [environ.core/env {:ehoks-url "example.com/path/"}
+                  oph.heratepalvelu.external.cas-client/get-service-ticket
                   mock-get-service-ticket
-                  oph.heratepalvelu.external.http-client/get mock-client-get
+                  oph.heratepalvelu.external.http-client/get mock-client-get]
+      (let [expected {:type "mock-client-call-response"
+                      :params {:method "get"
+                               :uri "example.com/path/hoks/opiskeluoikeus/1.2.3"
+                               :options
+                               {:headers {:ticket
+                                          {:type "cas-service-ticket"
+                                           :service "/ehoks-virkailija-backend"
+                                           :suffix "cas-security-check"}}
+                                :as :json}}}]
+        (is (= (ehoks/get-hoks-by-opiskeluoikeus "1.2.3") expected))))))
