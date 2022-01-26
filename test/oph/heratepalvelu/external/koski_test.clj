ed84207fe09bd244bfab9cec70527705ea69cfdd
Kelvin Jackson <kelvin.jackson@gofore.com>
2022-01-26 15:07:14 +0200

    Lisätty koski-testit

--- test/oph/heratepalvelu/external/koski_test.clj
+++ test/oph/heratepalvelu/external/koski_test.clj
@@ -29,5 +29,38 @@
                   mock-throws-other-error]
       (is (thrown? ExceptionInfo (koski/get-opiskeluoikeus-catch-404 "1.2"))))))
 
+(def test-get-updated-opiskeluoikeudet-saved-params (atom {}))
 
-;; TODO get-updated-opiskeluoikeudet
+(defn- mock-get-updated-opiskeluoikeudet-client-get [uri options]
+  (reset! test-get-updated-opiskeluoikeudet-saved-params {:uri uri
+                                                          :options options})
+  {:body [{:opiskeluoikeudet [{:aikaleima "2021-01-01"}
+                              {:aikaleima "2020-04-04"}]}
+          {:opiskeluoikeudet [{:aikaleima "2020-11-11"}
+                              {:aikaleima "2022-01-25"}
+                              {:aikaleima "2021-05-05"}]}]})
+
+(deftest test-get-updated-opiskeluoikeudet
+  (testing "get-updated-opiskeluoikeudet tekee oikean kutsun ja käsittelyn"
+    (with-redefs [environ.core/env {:koski-url "example.com/koski"
+                                    :koski-user "user"}
+                  oph.heratepalvelu.external.http-client/get
+                  mock-get-updated-opiskeluoikeudet-client-get
+                  oph.heratepalvelu.external.koski/pwd (delay "secret")]
+      (let [expected [{:aikaleima "2020-04-04"}
+                      {:aikaleima "2020-11-11"}
+                      {:aikaleima "2021-01-01"}
+                      {:aikaleima "2021-05-05"}
+                      {:aikaleima "2022-01-25"}]
+            expected-saved {:uri "example.com/koski/oppija/"
+                            :options
+                            {:query-params
+                             {"opiskeluoikeudenTyyppi" "ammatillinenkoulutus"
+                              "muuttunutJälkeen" "2021-12-15"
+                              "pageSize" 100
+                              "pageNumber" 3}
+                             :basic-auth ["user" "secret"]
+                             :as :json-strict}}]
+        (is (= expected (koski/get-updated-opiskeluoikeudet "2021-12-15" 3)))
+        (is (= expected-saved
+               @test-get-updated-opiskeluoikeudet-saved-params))))))
