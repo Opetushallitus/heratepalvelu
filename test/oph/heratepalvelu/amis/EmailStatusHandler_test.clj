4075c0e75f3f92519ea68709ac1268f253ad449a
Kelvin Jackson <kelvin.jackson@gofore.com>
2022-02-23 08:58:36 +0200

    Duplikoitu convert-email-status -funktio poistettu

--- test/oph/heratepalvelu/amis/EmailStatusHandler_test.clj
+++ test/oph/heratepalvelu/amis/EmailStatusHandler_test.clj
@@ -4,25 +4,6 @@
             [oph.heratepalvelu.amis.EmailStatusHandler :as esh]
             [oph.heratepalvelu.test-util :as tu]))
 
-(deftest test-convert-vp-email-status
-  (testing "Converts viestintäpalvelu email status to internal tila"
-    (let [status1 {:numberOfSuccessfulSendings 1
-                   :numberOfBouncedSendings 0
-                   :numberOfFailedSendings 0}
-          status2 {:numberOfSuccessfulSendings 0
-                   :numberOfBouncedSendings 1
-                   :numberOfFailedSendings 0}
-          status3 {:numberOfSuccessfulSendings 0
-                   :numberOfBouncedSendings 0
-                   :numberOfFailedSendings 1}
-          status4 {:numberOfSuccessfulSendings 0
-                   :numberOfBouncedSendings 0
-                   :numberOfFailedSendings 0}]
-      (is (= (:success c/kasittelytilat) (esh/convert-vp-email-status status1)))
-      (is (= (:bounced c/kasittelytilat) (esh/convert-vp-email-status status2)))
-      (is (= (:failed c/kasittelytilat) (esh/convert-vp-email-status status3)))
-      (is (= nil (esh/convert-vp-email-status status4))))))
-
 (def mock-send-lahetys-data-to-ehoks-results (atom {}))
 
 (defn- mock-send-lahetys-data-to-ehoks [toimija-oppija tyyppi-kausi data]
