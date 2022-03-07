6501f76fa72c45ee229baa41c1bdb7a12988d8bd
Kelvin Jackson <kelvin.jackson@gofore.com>
2022-03-07 09:41:09 +0200

    Integraatiotesti

--- test/oph/heratepalvelu/integration_tests/amis/AMISehoksTimedOperationsHandler_i_test.clj
+++ test/oph/heratepalvelu/integration_tests/amis/AMISehoksTimedOperationsHandler_i_test.clj
@@ -64,3 +64,79 @@
       (is (true? (tu/logs-contain? {:level :info
                                     :message "Lähetetty 2 viestiä"})))
       (teardown-test))))
+
+
+;; -handleMassHerateResend testi
+
+(defn- setup-mass-test []
+  (mcc/clear-results)
+  (mhc/clear-results)
+  (mhc/clear-url-bindings)
+  (mhc/bind-url
+    :post
+    (str (:ehoks-url mock-env) "hoks/resend-aloitusherate")
+    {:query-params {:from "2022-01-19" :to "2022-02-02"}
+     :as :json
+     :headers {:ticket
+               "service-ticket/ehoks-virkailija-backend/cas-security-check"}}
+    {:body {:data {:count 2}}})
+  (mhc/bind-url
+    :post
+    (str (:ehoks-url mock-env) "hoks/resend-paattoherate")
+    {:query-params {:from "2022-01-19" :to "2022-02-02"}
+     :as :json
+     :headers {:ticket
+               "service-ticket/ehoks-virkailija-backend/cas-security-check"}}
+    {:body {:data {:count 3}}}))
+
+(defn- teardown-mass-test []
+  (mcc/clear-results)
+  (mhc/clear-results)
+  (mhc/clear-url-bindings))
+
+(def expected-mass-http-results
+  [{:method :post
+    :url "https://oph-ehoks.com/hoks/resend-aloitusherate"
+    :options
+    {:headers {:ticket
+               "service-ticket/ehoks-virkailija-backend/cas-security-check"}
+     :query-params {:from "2022-01-19" :to "2022-02-02"}
+     :as :json}}
+   {:method :post
+    :url "https://oph-ehoks.com/hoks/resend-paattoherate"
+    :options
+    {:headers {:ticket
+               "service-ticket/ehoks-virkailija-backend/cas-security-check"}
+     :query-params {:from "2022-01-19" :to "2022-02-02"}
+     :as :json}}])
+
+(def expected-mass-cas-client-results [{:type :get-service-ticket
+                                        :service "/ehoks-virkailija-backend"
+                                        :suffix "cas-security-check"}
+                                       {:type :get-service-ticket
+                                        :service "/ehoks-virkailija-backend"
+                                        :suffix "cas-security-check"}])
+
+(deftest test-AMISMassHerateResend-integration
+  (testing "MassHerateResend integraatiotesti"
+    (with-redefs [clojure.tools.logging/log* tu/mock-log*
+                  environ.core/env mock-env
+                  oph.heratepalvelu.common/local-date-now
+                  (fn [] (LocalDate/of 2022 2 2))
+                  oph.heratepalvelu.external.cas-client/get-service-ticket
+                  mcc/mock-get-service-ticket
+                  oph.heratepalvelu.external.http-client/post mhc/mock-post]
+      (setup-mass-test)
+      (toh/-handleMassHerateResend {}
+                                   (tu/mock-handler-event :scheduledherate)
+                                   (tu/mock-handler-context))
+      (is (= (mcc/get-results) expected-mass-cas-client-results))
+      (is (= (mhc/get-results) expected-mass-http-results))
+      (is (true?
+            (tu/logs-contain?
+              {:level :info
+               :message "Käynnistetään herätteiden massauudelleenlähetys"})))
+      (is (true? (tu/logs-contain?
+                   {:level :info
+                    :message "Lähetetty 2 aloitusviestiä ja 3 päättöviestiä"})))
+      (teardown-mass-test))))
