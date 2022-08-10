74a30f72dc3fc9c0b8a08ef8003dedf123408f4e
samje <sami.honkanen@cybercom.com>
2022-08-10 13:04:41 +0300

    WIP

--- src/oph/heratepalvelu/util/dbChanger.clj
+++ src/oph/heratepalvelu/util/dbChanger.clj
@@ -1,14 +1,13 @@
-(ns oph.heratepalvelu.util.rahoitusryhmaJalkiajo
-  (:require [oph.heratepalvelu.common :as c]
-            [oph.heratepalvelu.db.dynamodb :as ddb]
+(ns oph.heratepalvelu.util.dbChanger
+  (:require [oph.heratepalvelu.db.dynamodb :as ddb]
             [environ.core :refer [env]]
-            [clojure.tools.logging :as log]
-            [oph.heratepalvelu.external.arvo :as arvo])
+            [clj-time.core :as t]
+            [clojure.tools.logging :as log])
   (:import (software.amazon.awssdk.services.dynamodb.model ScanRequest AttributeValue)))
 
 (gen-class
-  :name "oph.heratepalvelu.util.rahoitusryhmaJalkiajo"
-  :methods [[^:static handleRahoitusRyhmaJalkiajo
+  :name "oph.heratepalvelu.util.dbChanger"
+  :methods [[^:static handleDBUpdate
              [com.amazonaws.services.lambda.runtime.events.ScheduledEvent
               com.amazonaws.services.lambda.runtime.Context] void]])
 
@@ -21,19 +20,19 @@
                   (.exclusiveStartKey (:exclusive-start-key options))
                   (:expr-attr-vals options)
                   (.expressionAttributeValues (:expr-attr-vals options)))
-                (.tableName (:jaksotunnus-table env))
+                (.tableName (:table env))
                 (.build))
         response (.scan ddb/ddb-client req)]
     (log/info (count (.items response)))
     response))
 
-(defn -handleRahoitusRyhmaJalkiajo [this event context]
+(defn -handleDBUpdate [this event context]
   (loop [resp (scan {:filter-expression (str "attribute_not_exists(rahoitusryhma) "
-                                             "AND loppupvm >= :pvm ")
+                                             "AND alkupvm >= :pvm ")
                      :expr-attr-vals {":pvm" (.build (.s (AttributeValue/builder) "2022-07-01"))}})]
     (doseq [item (map ddb/map-attribute-values-to-vals (.items resp))]
       (try
-        (let [rahoitusryhma (c/get item)]
+        (let [rahoitusryhma (c/get-rah item)]
           (println rahoitusryhma)
           (if rahoitusryhma
             (ddb/update-item
@@ -43,12 +42,18 @@
                :expr-attr-vals {":value1" [:s rahoitusryhma]}}
               (:jaksotunnus-table env))
             (arvo/patch-vastaajatunnus (:tunnus item) {:rahoitusryhma rahoitusryhma})
-        (catch Exception e
-          (log/error e))))
+            (catch Exception e
+              (log/error e))))
+        (when (.hasLastEvaluatedKey resp)
+          (recur (scan {:exclusive-start-key (.lastEvaluatedKey resp)
+                        :filter-expression (str "attribute_not_exists(oppisopimuksen_perusta) "
+                                                "AND hankkimistapa_tyyppi = :htp "
+                                                "AND jakso_loppupvm >= :pvm "
+                                                "AND dbchangerin_kasittelema <> :dbc")
+                        :expr-attr-vals {":pvm" (.build (.s (AttributeValue/builder) "2022-07-01"))}}))))
     (when (.hasLastEvaluatedKey resp)
-      (recur (scan {:exclusive-start-key (.lastEvaluatedKey resp)
-                    :filter-expression (str "attribute_not_exists(oppisopimuksen_perusta) "
-                                            "AND hankkimistapa_tyyppi = :htp "
-                                            "AND jakso_loppupvm >= :pvm "
-                                            "AND dbchangerin_kasittelema <> :dbc")
-                    :expr-attr-vals {":pvm" (.build (.s (AttributeValue/builder) "2022-07-01"))}})))))))
+      (recur (scan
+               {filter-expression (str "attribute_not_exists(rahoitusryhma) "
+                                       "AND loppupvm >= :pvm ")
+                :expr-attr-vals {":value1" (.build (.s (AttributeValue/builder) "phone-mismatch"))}
+                :exclusive-start-key (.lastEvaluatedKey resp)})))))
