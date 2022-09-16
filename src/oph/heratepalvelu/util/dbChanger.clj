0d12ace0c97cc60d70e1c4919b6b31b54d78b836
samje <sami.honkanen@cybercom.com>
2022-09-16 17:01:01 +0300

    WIP

--- src/oph/heratepalvelu/util/dbChanger.clj
+++ src/oph/heratepalvelu/util/dbChanger.clj
@@ -109,7 +109,7 @@
     (when (.hasLastEvaluatedKey resp)
       (recur (scan
                {:filter-expression (str "jakso_loppupvm >= :start "
-                                        "AND jakso_loppupvm >= :end "
+                                        "AND jakso_loppupvm <= :end "
                                         "AND attribute_not_exists(uudelleenlaskettu_kesto)")
                 :expr-attr-vals {":start" (.build (.s (AttributeValue/builder) "2021-11-01"))
                                  ":end" (.build (.s (AttributeValue/builder) "2021-12-31"))}})))))
