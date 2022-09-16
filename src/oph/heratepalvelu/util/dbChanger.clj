96639fa9ac77e422eff1b306b6172ecfaaaca598
samih <sami.honkanen@cybercom.com>
2022-09-16 17:10:51 +0300

    Uudelleenlaskettu kesto, toinen kokeilu

--- src/oph/heratepalvelu/util/dbChanger.clj
+++ src/oph/heratepalvelu/util/dbChanger.clj
@@ -67,48 +67,49 @@
     (doseq [item (map ddb/map-attribute-values-to-vals (.items resp))]
       (try
         (let [dbjakso (ddb/query-items {:ohjaaja_ytunnus_kj_tutkinto [:eq [:s (:ohjaaja_ytunnus_kj_tutkinto item)]]}
-                                     {:index "niputusIndex"}
-                                     (:table env))
+                                       {:index "niputusIndex"}
+                                       (:table env))
               jakso (first dbjakso)
               ;;kesto (:kesto jakso)
               uudelleenlaskettu_kesto (:uudelleenlaskettu_kesto jakso)]
           (when (nil? uudelleenlaskettu_kesto)
             (do
-             (println (str "Lasketaan oppijan "
-                          (:oppija_oid jakso)
-                          " jaksojen kestot uudelleen niputuksessa "
-                          (:niputuspvm jakso)))
-            (let [oppijan-kaikki-jaksot (ddb/query-items
-                                        {:oppija_oid
-                                         [:eq [:s (:oppija_oid jakso)]]
-                                         :niputuspvm [:eq [:s (:niputuspvm jakso)]]}
-                                        {:index "tepDbChangerIndex"
-                                         :filter-expression "#pvm >= :pvm AND attribute_exists(#tunnus)"
-                                         :expr-attr-names {"#pvm"    "viimeinen_vastauspvm"
-                                                           "#tunnus" "tunnus"}
-                                         :expr-attr-vals {":pvm" [:s (:niputuspvm jakso)]}}
-                                        (:table env))
-                uudelleenlasketut-kestot (nip/compute-kestot oppijan-kaikki-jaksot)]
-              (doseq [jakso oppijan-kaikki-jaksot]
-                (println (str "Päivitetään jaksolle "
-                              (:hankkimistapa_id jakso)
-                              " uudelleenlaskettu_kesto."))
-                (println (str "Vanha kesto "
-                              (:kesto jakso)
-                              " - Uudelleen laskettu kesto "
-                              (nip/math-round (get uudelleenlasketut-kestot (:hankkimistapa_id jakso) 0.0))))
-                ;(let [uusi-kesto (nip/math-round (get uudelleenlasketut-kestot (:hankkimistapa_id jakso) 0.0))]
-                ;  (ddb/update-item {:hankkimistapa_id [:n (:hankkimistapa_id jakso)]}
-                ;                   (merge (c/create-update-item-options {:uudelleenlaskettu_kesto [:n uusi-kesto]}) {})
-                ;                   (:table env)))
-                )))))
+              (println (str "Lasketaan oppijan "
+                            (:oppija_oid jakso)
+                            " jaksojen kestot uudelleen niputuksessa "
+                            (:niputuspvm jakso)))
+              (let [oppijan-kaikki-jaksot (ddb/query-items
+                                            {:oppija_oid
+                                             [:eq [:s (:oppija_oid jakso)]]}
+                                            {:index "tepDbChangerIndex"
+                                             :filter-expression (str "jakso_loppupvm >= :start "
+                                                                     "jakso_loppupvm <= :end"
+                                                                     "AND attribute_exists(#tunnus)")
+                                             :expr-attr-names {"#tunnus" "tunnus"}
+                                             :expr-attr-vals {":start" (.build (.s (AttributeValue/builder) "2021-11-01"))
+                                                              ":end" (.build (.s (AttributeValue/builder) "2021-12-31"))}}
+                                            (:table env))
+                    uudelleenlasketut-kestot (nip/compute-kestot oppijan-kaikki-jaksot)]
+                (println uudelleenlasketut-kestot)
+                (doseq [jakso oppijan-kaikki-jaksot]
+                  (println (str "Päivitetään jaksolle "
+                                (:hankkimistapa_id jakso)
+                                " uudelleenlaskettu_kesto."))
+                  (println (str "Vanha kesto "
+                                (:kesto jakso)
+                                " - Uudelleen laskettu kesto "
+                                (nip/math-round (get uudelleenlasketut-kestot (:hankkimistapa_id jakso) 0.0))))
+                  ;(let [uusi-kesto (nip/math-round (get uudelleenlasketut-kestot (:hankkimistapa_id jakso) 0.0))]
+                  ;  (ddb/update-item {:hankkimistapa_id [:n (:hankkimistapa_id jakso)]}
+                  ;                   (merge (c/create-update-item-options {:uudelleenlaskettu_kesto [:n uusi-kesto]}) {})
+                  ;                   (:table env)))
+                  )))))
         (catch Exception e
           (log/error e))))
     (when (.hasLastEvaluatedKey resp)
       (recur (scan
                {:filter-expression (str "jakso_loppupvm >= :start "
-                                        "AND jakso_loppupvm >= :end "
+                                        "AND jakso_loppupvm <= :end "
                                         "AND attribute_not_exists(uudelleenlaskettu_kesto)")
                 :expr-attr-vals {":start" (.build (.s (AttributeValue/builder) "2021-11-01"))
                                  ":end" (.build (.s (AttributeValue/builder) "2021-12-31"))}})))))
-
