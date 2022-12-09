940625d45b540f153d83f4e062402f0fb8577546
Tomi Katajanheimo <tomi@katajanheimo.com>
2022-12-09 12:43:27 +0200

    Add tests

--- test/oph/heratepalvelu/integration_tests/amis/AMISehoksTimedOpsHandler_i_test.clj
+++ test/oph/heratepalvelu/integration_tests/amis/AMISehoksTimedOpsHandler_i_test.clj
@@ -1,12 +1,25 @@
 (ns oph.heratepalvelu.integration-tests.amis.AMISehoksTimedOpsHandler-i-test
   (:require [clojure.test :refer :all]
+            [oph.heratepalvelu.db.dynamodb :as ddb]
             [oph.heratepalvelu.amis.AMISehoksTimedOperationsHandler :as toh]
             [oph.heratepalvelu.integration-tests.mock-cas-client :as mcc]
             [oph.heratepalvelu.integration-tests.mock-http-client :as mhc]
-            [oph.heratepalvelu.test-util :as tu])
+            [oph.heratepalvelu.test-util :as tu]
+            [oph.heratepalvelu.integration-tests.mock-db :as mdb])
   (:import (java.time LocalDate)))
 
-(def mock-env {:ehoks-url "https://oph-ehoks.com/"})
+(def mock-env {:ehoks-url "https://oph-ehoks.com/"
+               :herate-table "herate-table"})
+
+(def starting-table-contents [{:ehoks-id [:n 1]
+                               :sahkoposti [:s "oppilas1@gmail.com"]
+                               :puhelinnumero [:s "0402222222"]}
+                              {:ehoks-id [:n 2]
+                               :sahkoposti [:s "oppilas2@gmail.com"]
+                               :puhelinnumero [:s "0402222223"]}
+                              {:ehoks-id [:n 3]
+                               :sahkoposti [:s "oppilas3@gmail.com"]
+                               :puhelinnumero [:s "0402222224"]}])
 
 (defn- setup-test []
   (mcc/clear-results)
@@ -21,12 +34,26 @@
      :as :json
      :headers {:ticket
                "service-ticket/ehoks-virkailija-backend/cas-security-check"}}
-    {:body {:data 2}}))
+    {:body {:data 2}})
+  (mhc/bind-url :delete
+                (str (:ehoks-url mock-env)
+                     "heratepalvelu/opiskelijan-yhteystiedot")
+                {:as :json
+                 :headers
+                 {:ticket
+                  "service-ticket/ehoks-virkailija-backend/cas-security-check"}}
+                {:body {:data {:hoks-ids [1 2 3]}}})
+  (mdb/clear-mock-db)
+  (mdb/create-table (:herate-table mock-env)
+                    {:primary-key :ehoks-id
+                     :sort-key :ehoks-id})
+  (mdb/set-table-contents (:herate-table mock-env) starting-table-contents))
 
 (defn- teardown-test []
   (mcc/clear-results)
   (mhc/clear-results)
-  (mhc/clear-url-bindings))
+  (mhc/clear-url-bindings)
+  (mdb/clear-mock-db))
 
 (def expected-http-results
   [{:method :get
@@ -37,9 +64,18 @@
      :query-params {:start "2021-07-01"
                     :end "2022-02-02"
                     :limit 1000}
+     :as :json}}
+   {:method :delete
+    :url "https://oph-ehoks.com/heratepalvelu/opiskelijan-yhteystiedot"
+    :options
+    {:headers {:ticket
+               "service-ticket/ehoks-virkailija-backend/cas-security-check"}
      :as :json}}])
 
 (def expected-cas-client-results [{:type :get-service-ticket
+                                   :service "/ehoks-virkailija-backend"
+                                   :suffix "cas-security-check"}
+                                  {:type :get-service-ticket
                                    :service "/ehoks-virkailija-backend"
                                    :suffix "cas-security-check"}])
 
@@ -51,7 +87,9 @@
                   (fn [] (LocalDate/of 2022 2 2))
                   oph.heratepalvelu.external.cas-client/get-service-ticket
                   mcc/mock-get-service-ticket
-                  oph.heratepalvelu.external.http-client/get mhc/mock-get]
+                  oph.heratepalvelu.external.http-client/get mhc/mock-get
+                  oph.heratepalvelu.external.http-client/delete mhc/mock-delete
+                  ddb/update-item mdb/update-item]
       (setup-test)
       (toh/-handleAMISTimedOperations {}
                                       (tu/mock-handler-event :scheduledherate)
@@ -63,6 +101,24 @@
                     :message "Käynnistetään herätteiden lähetys"})))
       (is (true? (tu/logs-contain? {:level :info
                                     :message "Lähetetty 2 viestiä"})))
+
+      (is (true? (tu/logs-contain?
+                  {:level :info
+                   :message
+                   "Käynnistetään opiskelijan yhteystietojen poisto"})))
+      (is (true? (tu/logs-contain?
+                  {:level :info
+                   :message "Poistettu 3 opiskelijan yhteystiedot"})))
+      (is (= (mdb/get-table-values (:herate-table mock-env))
+             #{{:ehoks-id [:n 1]
+                :sahkoposti [:s nil]
+                :puhelinnumero [:s nil]}
+               {:ehoks-id [:n 2]
+                :sahkoposti [:s nil]
+                :puhelinnumero [:s nil]}
+               {:ehoks-id [:n 3]
+                :sahkoposti [:s nil]
+                :puhelinnumero [:s nil]}}))
       (teardown-test))))
 
 ;; -handleMassHerateResend testi
