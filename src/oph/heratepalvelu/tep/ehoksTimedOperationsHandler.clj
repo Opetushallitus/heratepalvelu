aa4b9ad1477562ca61e0f85bdd0ae05036638231
Tomi Katajanheimo <tomi@katajanheimo.com>
2023-01-23 13:10:13 +0200

    Poista yhteystiedot vain kannassa olevilta riveiltä

--- src/oph/heratepalvelu/tep/ehoksTimedOperationsHandler.clj
+++ src/oph/heratepalvelu/tep/ehoksTimedOperationsHandler.clj
@@ -25,16 +25,24 @@
   (log/info "Käynnistetään työpaikkaohjaajan yhteystietojen poisto")
   (let [hankkimistavat
         (get-in (ehoks/delete-tyopaikkaohjaajan-yhteystiedot)
-                [:body :data :hankkimistapa-ids])]
+                [:body :data :hankkimistapa-ids])
+        counter (atom 0)]
     (doseq [hankkimistapa_id hankkimistavat]
-      (log/info "Poistetaan ohjaajan yhteystiedot (hankkimistapa_id = "
-                hankkimistapa_id
-                ")")
-      (ddb/update-item
-       {:hankkimistapa_id [:n hankkimistapa_id]}
-       {:update-expr "SET #eml = :eml_value, #puh = :puh_value"
-        :expr-attr-names {"#eml" "ohjaaja_email"
-                          "#puh" "ohjaaja_puhelinnumero"}
-        :expr-attr-vals {":eml_value" [:s ""] ":puh_value" [:s ""]}}
-       (:jaksotunnus-table env)))
-    (log/info "Poistettu" (count hankkimistavat) "ohjaajan yhteystiedot")))
+      (let [resp (ddb/scan {:filter-expression   "#id = :id"
+                            :expr-attr-names     {"#id" "hankkimistapa_id"}
+                            :expr-attr-vals      {":id" [:n hankkimistapa_id]}}
+                           (:jaksotunnus-table env))
+            items (:items resp)]
+        (doseq [item items]
+          (log/info "Poistetaan ohjaajan yhteystiedot (hankkimistapa_id = "
+                    hankkimistapa_id
+                    ")")
+          (ddb/update-item
+            {:hankkimistapa_id [:n hankkimistapa_id]}
+            {:update-expr "SET #eml = :eml_value, #puh = :puh_value"
+             :expr-attr-names {"#eml" "ohjaaja_email"
+                               "#puh" "ohjaaja_puhelinnumero"}
+             :expr-attr-vals {":eml_value" [:s ""] ":puh_value" [:s ""]}}
+            (:jaksotunnus-table env))
+          (swap! counter inc))))
+    (log/info "Poistettu" @counter "ohjaajan yhteystiedot")))
