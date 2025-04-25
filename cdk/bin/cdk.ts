6d70445ceddb333f76a03f4bb63b367f39ace978
Severi Jääskeläinen <severi.jaaskelainen@gofore.com>
2025-04-25 14:08:21 +0300

    Remove old temporary TEPRAHOITUSStack

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,7 +3,6 @@ import { App } from "aws-cdk-lib";
 import { HeratepalveluAMISStack } from "../lib/amis";
 import { HeratepalveluTEPStack } from "../lib/tep";
 import { HeratepalveluTPKStack } from "../lib/tpk";
-import { HeratepalveluTEPRAHOITUSStack } from "../lib/teprah";
 import { simpleGit, SimpleGitOptions } from "simple-git";
 
 const checkGitStatus = async () => {
@@ -29,19 +28,16 @@ const checkGitStatus = async () => {
 
   new HeratepalveluAMISStack(app, "sieni-services-heratepalvelu", 'sieni', version);
   const tepSieni = new HeratepalveluTEPStack(app, "sieni-services-heratepalvelu-tep", 'sieni', version);
-  new HeratepalveluTEPRAHOITUSStack(app, "sieni-services-heratepalvelu-teprah", 'sieni', version, tepSieni.jaksotunnusTable);
   new HeratepalveluTPKStack(app, "sieni-services-heratepalvelu-tpk", 'sieni', version, tepSieni.jaksotunnusTable);
 
   if (status.isClean() && status.ahead === 0 && status.behind === 0) {
     new HeratepalveluAMISStack(app, "pallero-services-heratepalvelu", 'pallero', version);
     const tepPallero = new HeratepalveluTEPStack(app, "pallero-services-heratepalvelu-tep", 'pallero', version);
-    new HeratepalveluTEPRAHOITUSStack(app, "pallero-services-heratepalvelu-teprah", 'pallero', version, tepPallero.jaksotunnusTable);
     new HeratepalveluTPKStack(app, "pallero-services-heratepalvelu-tpk", 'pallero', version, tepPallero.jaksotunnusTable);
 
     if (status.tracking === "origin/master") {
       new HeratepalveluAMISStack(app, "sade-services-heratepalvelu", 'sade', version);
       const tepSade = new HeratepalveluTEPStack(app, "sade-services-heratepalvelu-tep", 'sade', version);
-      new HeratepalveluTEPRAHOITUSStack(app, "sade-services-heratepalvelu-teprah", 'sade', version, tepSade.jaksotunnusTable);
       new HeratepalveluTPKStack(app, "sade-services-heratepalvelu-tpk", 'sade', version, tepSade.jaksotunnusTable);
     } else {
       console.log("\nNOT IN MASTER BRANCH!!!\n")
